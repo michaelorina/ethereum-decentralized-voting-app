d25c73a4671b16f220efef736c86576bad687ff2
michaelorina <orinamichael4@gmail.com>
2023-06-12 19:49:11 +0300

    define variables in voter.js

--- context/Voter.js
+++ context/Voter.js
@@ -12,14 +12,32 @@ import {VotingAddress, VotingAdressABI} from "./constants";
 
 const client = ipfsHttpClient('https://ipfs.infura.io:5001/api/v0');
 
-const fetchContract = (signerOrProvider) =>
-    new ethers.Contract(VotingAddress, VotingAddressABI, signerOrProvider); 
+const fetchContract = (signerOrProvider) => new ethers.Contract(VotingAddress, VotingAddressABI, signerOrProvider); 
 
 export const VotingContext = React.createContext();
 
 export const VotingProvider = ({children}) => {
 
-    const votingTitle = "My First smart contract app";
+    const votingTitle = 'My First smart contract app';
+    const router = useRouter();
+    const [currentAccount, setCurrentAccount] = useState('');
+    const [candidateLength, setCandidateLength] = useState('');
+    const pushCandidate = [];
+    const candidateIndex = [];
+    const [candidateArray, setCandidateArray] = useState(pushCandidate);
+
+    //--------- END OF CANDIDATE DATA
+
+    const [error, setError] = useState('');
+    const highestVote = [];
+
+    //-------- VOTER SECTION
+    const pushVoter = [];
+    const [voterArray, setVoterArray] = useState(pushVoter);
+    const [voterLenght, setVoterLength] = useState('');
+    const [voterAddress, setVoterAddress] = useState([]);
+    
+
     return (
         <VotingContext.Provider value={{votingTitle}}>
             {children}
--- pages/_app.js
+++ pages/_app.js
@@ -4,15 +4,15 @@ import '../styles/globals.css';
 import {VotingProvider} from "../context/Voter";
 import NavBar from "../components/NavBar/NavBar";
 
-const myApp = ({ Component, pageProps})=>(
+const MyApp = ({ Component, pageProps})=>(
     <VotingProvider>
         <div>
             <NavBar />
             <div>
-                < Component {...pageProps} />
+                < Component {...pageProps} />;
             </div>
         </div>
     </VotingProvider>
 );
 
-export default myApp;
+export default MyApp;
