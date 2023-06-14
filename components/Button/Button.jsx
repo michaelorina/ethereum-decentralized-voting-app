427f93f1656a0acad05700d0f1e69fbc4719cae2
michaelorina <orinamichael4@gmail.com>
2023-06-14 16:01:46 +0300

    allowedVoters part

---
+++ components/Button/Button.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+const Button = () => {
+    <div>
+        Button
+    </div>
+}
+
+export default Button;
--- pages/allowd-voters.js
+++ pages/allowd-voters.js
@@ -1,9 +1,43 @@
-import React from 'react'
+import React, {useState, useEffect, useCallback, useContext} from 'react';
+import { useRouter } from 'next/router';
+import { useDropzone } from 'react-dropzone';
+import Image from 'next/image';
 
-const allowdVoters = () => {
-  return (
-    <div>allowd-voters</div>
-  )
+
+//---INTERNAL IMPORTS
+import {VotingContext} from "../context/Voter";
+import Style from "../styles/allowdVoters.module.css";
+import images from "../assets";
+import Button from "../components/Button/Button";
+import Input from "../components/Input/Input";
+
+const allowedVoters = () =>{
+  const [fileUrl, setFileurl] = useState(null);
+  const [formInput, setFormInput] = useState({
+    name: "",
+    address: "",
+    position: "",
+  });
+
+  const router = useRouter();
+  const {uploadToIPFS} = useContext(VotingContext);
+
+  //-- --- VOTERS IMAGE DROP
+  const onDrop = useCallback(async(acceptedFil) => {
+    const url = await uploadToIPFS(acceptedFil[0]);
+    setFileurl(url);
+  });
+
+  const {getRootProps, getInputProps} = useDropzone ({
+    onDrop,
+    accept: "image/*",
+    maxSize: 5000000,
+  });
+
+
+  
+  return <div></div>;
 }
 
-export default allowdVoters
+
+export default allowedVoters;
